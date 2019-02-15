6b44e994f4d108aef8ead4855b13e08ed9e2b9f3
Node <diwaasok@gmail.com>
2019-02-15 16:02:18 +0530

    code change

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,11 +3,7 @@ class StudentsController < ApplicationController
 	def new
 		@student = Student.new
 		@students = Student.all
-		if params[:id] != nil and params[:id] != ""
-			@student_destroy = Student.destroy(params[:id])
-		end
 	end
-	
 	def create
 		@student = Student.create(user_params)
 		if @student.save
@@ -17,7 +13,6 @@ class StudentsController < ApplicationController
 
 
 	private
-
   def user_params
     params.require(:student).permit(:first_name, :last_name, :age, :department)
   end
